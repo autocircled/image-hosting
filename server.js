de1094c4db468a9ad7a8ea528dad84235e33c120
Moktadir Rahman <autocircled@gmail.com>
2025-11-25 22:39:10 +0600

    git init

---
+++ server.js
@@ -0,0 +1,31 @@
+// server.js
+const express = require('express');
+const multer = require('multer');
+const cors = require('cors');
+// const path = require('path');
+
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log("request are coming...", req.file);
+    try{
+        res.json({
+            message: 'Image uploaded successfully',
+            filename: req.file.filename,
+            originalname: req.file.originalname
+        });
+    } catch(error){
+        console.log("Failed to upload image");
+    }
+});
+
+app.get('/', (req, res) => {
+    res.status(200).send('Server is running');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
